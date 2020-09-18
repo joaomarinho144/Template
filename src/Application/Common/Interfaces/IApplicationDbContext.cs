9934f9eb60a70e619226f4104462b493783b9a17
Lucas Oliveira <olucas16@gmail.com>
2020-09-17 22:35:30 -0300

    Template

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ src/Domain/Entities/Nota.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+using System;
+
+namespace Domain.Entities
+{
+    public class Nota : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
